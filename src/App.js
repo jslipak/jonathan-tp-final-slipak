eca59012d5d8ecdfec08301803791073ab785663
Jonathan Slipak <seriedigital@gmail.com>
2021-05-28 08:42:35 -0300

    entrega segunda NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarTest from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBarTest />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
