f2371a5fc90555df7f84ffffbade931bd396921a
Jonathan Slipak <seriedigital@gmail.com>
2021-05-30 15:57:49 -0300

    new NavBar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import './NavBar.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-
+import logo from '../images/80th food-logos_transparent.png'
 export default function NavBar(){
     return (
-<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+<Navbar collapseOnSelect expand="lg" className="Nav-Style" variant="dark">
+  <Navbar.Brand href="#home"><img src={logo} alt="logo80thfood" className="Logo-Brand" /></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#features">Alfajoras</Nav.Link>
+      <Nav.Link href="#pricing">Caramelos</Nav.Link>
+      <Nav.Link href="#deets">Chicles</Nav.Link>
+      <Nav.Link href="#memes">Chocolates</Nav.Link>
+      <Nav.Link href="#choco">Varios</Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -17,12 +21,6 @@ export default function NavBar(){
         <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
