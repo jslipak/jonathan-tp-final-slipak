6738fd2e26b203933fd098bcd2aa5479d7afb2c5
Jonathan Slipak <seriedigital@gmail.com>
2021-07-03 20:26:06 -0300

    just ending first review firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import '@firebase/firestore';
+
+const app = firebase.initializeApp({
+  apiKey: 'AIzaSyCzs_ckTS_ONOG0cZQpa0bftafmJCQTEJk',
+  authDomain: 'candystore-jls.firebaseapp.com',
+  projectId: 'candystore-jls',
+  storageBucket: 'candystore-jls.appspot.com',
+  messagingSenderId: '411951059473',
+  appId: '1:411951059473:web:551466f4db4eaf699717b0',
+});
+
+export function getFirebase() {
+  return app;
+}
+
+export function getFirestore() {
+  return firebase.firestore(app);
+}
